640ac9a243d0e5bd870bc06ed4f2a644e1674d89
muhammadzumair <zumairmuhammad@gmai.com>
2018-05-09 22:25:13 +0500

    feedback get

--- src/views/ReasonAnalytics.js
+++ src/views/ReasonAnalytics.js
@@ -1,52 +1,104 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import { Input, Container, Col, Row, Button } from 'reactstrap';
+import InformationBox from '../components/InformationBox';
+import * as firebase from 'firebase';
+
+
 
 export default class ReasonAnalytics extends Component {
-  render() {
-    return (
-      <div style={styles.container}>
-        <Container>
-          <Row>
-            asdjklasdjhasdasdjhkasdjkasdjhkasdasd
-          </Row>
-        </Container>
-      </div>
-    )
+  constructor() {
+    super();
+    this.state = {
+      location: 'tariq-road',
+      acount: null,
+      ecount: null,
+      wcount: null
+    }
   }
-}
 
-const styles = {
-  container: {
-    display: "flex",
-    flexWrap: "wrap",
-    flex: 1,
-    width: '100%',
-    height: '100vh',
-    overflow: 'hidden',
-    backgroundColor: '#efefef',
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  input: {
-    width: 300,
-    padding: 10,
-    borderWidth: 0,
-    margin: "5px auto",
-    outline: "none"
-  },
-  logo: {
-    marginTop: -150,
-    display: "block",
-    margin: "auto",
-    width: "100%",
-    maxWidth: 250,
-    height: "auto",
-    alignSelf: "center"
-  },
-  btn: {
-    display: "block",
-    margin: "20px auto",
-    padding: "10px 50px",
+  componentWillMount() {
+    const AttRef = firebase.database().ref().child(this.state.location).child('angry').child('{pushId}').child('reason');
+    AttRef.orderByChild('timeStamp').startAt(new Date().setHours(0,0,0,0)).on('value', snap => {
+      this.setState({
+        acount: snap.numChildren()
+      })
+    })
+
+    const EnvRef = firebase.database().ref().child(this.state.location).child('angry');
+    EnvRef.orderByChild('timeStamp').startAt(new Date().setHours(0,0,0,0)).on('value', snap => {
+      this.setState({
+        ecount: snap.numChildren()
+      })
+    })
+
+    const WaitingRef = firebase.database().ref().child(this.state.location).child('dissatisfied');
+    WaitingRef.orderByChild('timeStamp').startAt(new Date().setHours(0,0,0,0)).on('value', snap => {
+      this.setState({
+        wcount: snap.numChildren()
+      })
+    })
+  }
+
+  render() {
+    let messages = []
+    const happyRef = firebase.database().ref().child(this.state.location).child('angry');
+    happyRef.on('value', snap => {
+      snap.forEach(childSnap => {
+        if (childSnap.val().comment !== undefined) {
+          messages.push({
+            comment: childSnap.val().comment,
+            time: childSnap.val().comment
+          })
+        }
+      })
+    })
+
+    const Message = ({ mesg, date }) => {
+      return (
+        <div>
+          <div>{mesg}</div>
+        </div>
+      )
+    }
+    return (
+      <Container className="animated fadeIn">
+      <Row>
+        
+      </Row>
+        <Row>
+          <Col xs="12" sm="6" md="3">
+            <Row>
+              <InformationBox
+                title="Bad Attitude"
+                numbers={this.state.acount}
+                iconURL={require('../assets/attitude.png')}
+              />
+            </Row>
+            <Row>
+              <InformationBox
+                title="Environment"
+                numbers={this.state.ecount}
+                iconURL={require('../assets/environment.png')}
+              />
+            </Row>
+            <Row>
+              <InformationBox
+                title="Waiting Time"
+                numbers={this.state.wcount}
+                iconURL={require('../assets/waiting-time.png')}
+              />
+            </Row>
+          </Col>
+          <Col xl="6" md="6" xs="12">
+            <Row>
+              <Container>
+                {messages.map(({ comment, time }) => <Message mesg={comment} date={time} />)}
+              </Container>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    )
   }
 }
